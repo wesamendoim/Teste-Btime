8ba7a27449ca0809dbccb70a53b81a1bb36c4364
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2025-08-29 09:20:49 -0300

    Script_V2 com arquivo CSV

--- Script_V2.py
+++ Script_V2.py
@@ -2,14 +2,54 @@ import requests
 import pandas as pd
 
 try:
-    endpoint = "https://www.espn.com.br/futebol/classificacao/_/liga/bra.1"
+    # Conectando na API da ESPN
+    endpoint = "https://site.api.espn.com/apis/v2/sports/soccer/bra.1/standings"
 
+    #Pedindo a conexão
     response = requests.get(endpoint)
 
+    #Verificando se a conexão foi bem sucedida
     if response.status_code == 200:
         print("Conexão bem sucedida!")  
     else:
         print(f"Falha na conexão. Código de status: {response.status_code}")
+
+    data = response.json()
+    df = pd.json_normalize(data["children"][0]["standings"]["entries"], sep=".")
+    print(df)
+    
+    entries = pd.json_normalize(
+    data["children"][0]["standings"]["entries"],
+    sep=".")
+    
+    #Variavel para guardar a tabela
+    tabela = []
+
+    #Iterando sobre as linhas do Json
+    for _, row in entries.iterrows():
+        team = row["team.displayName"] #Capturando o nome do time
+        stats_list = row["stats"] #Capturando a lista de stats
+        stats = {s["name"]: s.get("value", s.get("displayValue")) for s in stats_list}
+
+        #Montando a tabela para enviar para o DataFrame
+        tabela.append({
+            "time": team,
+            "jogos": stats.get("gamesPlayed"),
+            "vitorias": stats.get("wins"),
+            "empates": stats.get("ties"),
+            "derrotas": stats.get("losses"),
+            "gols_pro": stats.get("pointsFor"),
+            "gols_contra": stats.get("pointsAgainst"),
+            "saldo": stats.get("pointDifferential"),
+            "pontos": stats.get("points"),
+        })
+
+    #Montando o DF
+    df = pd.DataFrame(tabela)
+    print(df)
+
+    #Montando o CSV
+    df.to_csv("Excel(arquivocsv)\\tabela_brasileirao_V2.csv", index=False, sep=";", encoding="utf-8-sig")
 
 except Exception as e:
     print("Erro ao conectar: ", e)
